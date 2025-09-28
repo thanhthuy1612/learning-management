b747a9a4cd6de7ea94a7fec4604eb5121e60ba02
sophia <thanhthuy16122001@gmail.com>
2025-09-28 23:42:54 +0700

    fix bug

--- src/layouts/dashboard/layout.tsx
+++ src/layouts/dashboard/layout.tsx
@@ -136,40 +136,12 @@ export function DashboardLayout({
           {isNavHorizontal && (
             <VerticalDivider sx={{ [theme.breakpoints.up(layoutQuery)]: { display: 'flex' } }} />
           )}
-
-          {/** @slot Workspace popover */}
-          {/* <WorkspacesPopover
-            data={_workspaces}
-            sx={{ ...(isNavHorizontal && { color: 'var(--layout-nav-text-primary-color)' }) }}
-          /> */}
         </>
       ),
       rightArea: (
         <Box sx={{ display: 'flex', alignItems: 'center', gap: { xs: 0, sm: 0.75 } }}>
-          {/** @slot Searchbar */}
           <Searchbar data={navData} />
-
-          {/** @slot Language popover */}
-          {/* <LanguagePopover
-            data={[
-              { value: 'en', label: 'English', countryCode: 'GB' },
-              { value: 'fr', label: 'French', countryCode: 'FR' },
-              { value: 'vi', label: 'Vietnamese', countryCode: 'VN' },
-              { value: 'cn', label: 'Chinese', countryCode: 'CN' },
-              { value: 'ar', label: 'Arabic', countryCode: 'SA' },
-            ]}
-          /> */}
-
-          {/** @slot Notifications popover */}
-          {/* <NotificationsDrawer data={_notifications} /> */}
-
-          {/** @slot Contacts popover */}
-          {/* <ContactsPopover data={_contacts} /> */}
-
-          {/** @slot Settings button */}
           <SettingsButton />
-
-          {/** @slot Account drawer */}
           <AccountDrawer data={_account} />
         </Box>
       ),
@@ -216,7 +188,7 @@ export function DashboardLayout({
       /** **************************************
        * @Sidebar
        *************************************** */
-      sidebarSection={isNavHorizontal ? null : renderSidebar()}
+      sidebarSection={renderSidebar()}
       /** **************************************
        * @Footer
        *************************************** */
--- src/layouts/dashboard/nav-mobile.tsx
+++ src/layouts/dashboard/nav-mobile.tsx
@@ -13,7 +13,6 @@ import { Scrollbar } from 'src/components/scrollbar';
 import { NavSectionVertical } from 'src/components/nav-section';
 
 import { layoutClasses } from '../core/classes';
-import { NavUpgrade } from '../components/nav-upgrade';
 
 // ----------------------------------------------------------------------
 
@@ -76,7 +75,7 @@ export function NavMobile({
           sx={{ px: 2, flex: '1 1 auto' }}
           {...other}
         />
-        <NavUpgrade />
+        {/* <NavUpgrade /> */}
       </Scrollbar>
 
       {slots?.bottomArea}
