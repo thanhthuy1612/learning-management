c77338c67e1150ab90e7bb89411b44bf9c8f8c1f
sophia <thanhthuy16122001@gmail.com>
2025-10-08 12:06:37 +0700

    fix bug

--- src/auth/guard/guest-guard.tsx
+++ src/auth/guard/guest-guard.tsx
@@ -2,9 +2,8 @@
 
 import React, { useState, useEffect } from 'react';
 
+import { paths } from 'src/routes/paths';
 import { useRouter, useSearchParams } from 'src/routes/hooks';
-
-import { CONFIG } from 'src/global-config';
 
 import { SplashScreen } from 'src/components/loading-screen';
 
@@ -22,7 +21,7 @@ export function GuestGuard({ children }: GuestGuardProps) {
   const { loading, authenticated, user } = useAuthContext();
 
   const searchParams = useSearchParams();
-  const returnTo = searchParams.get('returnTo') || CONFIG.auth.redirectPath;
+  const returnTo = searchParams.get('returnTo') || paths.dashboard.root;
 
   const [isChecking, setIsChecking] = useState(true);
 
